758a330df9b18c847341d732053ef06985f95058
waminet <wamiwami@live.jp>
2020-06-23 11:17:41 +0900

    fix build script program

--- .github/scripts/on-push.sh
+++ .github/scripts/on-push.sh
@@ -18,8 +18,8 @@ else
 	export GITHUB_REPOSITORY="espressif/arduino-esp32"
 fi
 
-echo GITHUB_WORKSPACE
-echo GITHUB_REPOSITORY
+echo $GITHUB_WORKSPACE
+echo $GITHUB_REPOSITORY
 
 CHUNK_INDEX=$1
 CHUNKS_CNT=$2
@@ -42,17 +42,11 @@ if [ "$BUILD_PIO" -eq 0 ]; then
 	source ./.github/scripts/install-arduino-ide.sh
 	source ./.github/scripts/install-arduino-core-esp32.sh
 	if [ "$OS_IS_WINDOWS" == "1" ]; then
-		build_sketch "$FQBN" "$ARDUINO_ESP32_PATH/libraries/WiFiClientSecure/examples/WiFiClientSecure/WiFiClientSecure.ino" && \
-		build_sketch "$FQBN" "$ARDUINO_ESP32_PATH/libraries/BLE/examples/BLE_server/BLE_server.ino" && \
-		build_sketch "$FQBN" "$ARDUINO_ESP32_PATH/libraries/AzureIoT/examples/GetStarted/GetStarted.ino" && \
-		build_sketch "$FQBN" "$ARDUINO_ESP32_PATH/libraries/ESP32/examples/Camera/CameraWebServer/CameraWebServer.ino"
+		build_sketch "$FQBN" "$ARDUINO_ESP32_PATH/libraries/obniz/examples/led_blink/led_blink.ino" && \
+		build_sketch "$FQBN" "$ARDUINO_ESP32_PATH/libraries/obniz/examples/setup_key/setup_key.ino"
 	elif [ "$OS_IS_MACOS" == "1" ]; then
-		build_sketch "$FQBN" "$ARDUINO_ESP32_PATH/libraries/WiFi/examples/WiFiClient/WiFiClient.ino" && \
-		build_sketch "$FQBN" "$ARDUINO_ESP32_PATH/libraries/WiFiClientSecure/examples/WiFiClientSecure/WiFiClientSecure.ino" && \
-		build_sketch "$FQBN" "$ARDUINO_ESP32_PATH/libraries/BluetoothSerial/examples/SerialToSerialBT/SerialToSerialBT.ino" && \
-		build_sketch "$FQBN" "$ARDUINO_ESP32_PATH/libraries/BLE/examples/BLE_server/BLE_server.ino" && \
-		build_sketch "$FQBN" "$ARDUINO_ESP32_PATH/libraries/AzureIoT/examples/GetStarted/GetStarted.ino" && \
-		build_sketch "$FQBN" "$ARDUINO_ESP32_PATH/libraries/ESP32/examples/Camera/CameraWebServer/CameraWebServer.ino"
+		build_sketch "$FQBN" "$ARDUINO_ESP32_PATH/libraries/obniz/examples/led_blink/led_blink.ino" && \
+		build_sketch "$FQBN" "$ARDUINO_ESP32_PATH/libraries/obniz/examples/setup_key/setup_key.ino"
 	else
 		# CMake Test
 		if [ "$CHUNK_INDEX" -eq 0 ]; then
@@ -64,11 +58,7 @@ else
 	# PlatformIO Test
 	source ./.github/scripts/install-platformio-esp32.sh
 	BOARD="esp32dev"
-	build_pio_sketch "$BOARD" "$PLATFORMIO_ESP32_PATH/libraries/WiFi/examples/WiFiClient/WiFiClient.ino" && \
-	build_pio_sketch "$BOARD" "$PLATFORMIO_ESP32_PATH/libraries/WiFiClientSecure/examples/WiFiClientSecure/WiFiClientSecure.ino" && \
-	build_pio_sketch "$BOARD" "$PLATFORMIO_ESP32_PATH/libraries/BluetoothSerial/examples/SerialToSerialBT/SerialToSerialBT.ino" && \
-	build_pio_sketch "$BOARD" "$PLATFORMIO_ESP32_PATH/libraries/BLE/examples/BLE_server/BLE_server.ino" && \
-	build_pio_sketch "$BOARD" "$PLATFORMIO_ESP32_PATH/libraries/AzureIoT/examples/GetStarted/GetStarted.ino" && \
-	build_pio_sketch "$BOARD" "$PLATFORMIO_ESP32_PATH/libraries/ESP32/examples/Camera/CameraWebServer/CameraWebServer.ino"
+	build_pio_sketch "$BOARD" "$PLATFORMIO_ESP32_PATH/libraries/obniz/examples/led_blink/led_blink.ino" && \
+	build_pio_sketch "$BOARD" "$PLATFORMIO_ESP32_PATH/libraries/obniz/examples/setup_key/setup_key.ino" 
 	#build_pio_sketches esp32dev "$PLATFORMIO_ESP32_PATH/libraries"
 fi
