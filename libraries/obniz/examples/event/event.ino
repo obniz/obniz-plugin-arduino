b92c8e020c27c1722fa4a2b2b3b8262bba189312
waminet <wamiwami@live.jp>
2020-06-25 22:46:27 +0900

    add swich case

--- libraries/obniz/examples/event/event.ino
+++ libraries/obniz/examples/event/event.ino
@@ -35,6 +35,16 @@ void onEvent(os_event_t event, uint8_t* data, uint16_t length) {
   case PLUGIN_EVENT_NETWORK_CLOUD_DISCONNECTED:
     Serial.println("cloud Disconnected");
     break;
+  case PLUGIN_EVENT_NETWORK_WIFI_SCANNING:
+  case PLUGIN_EVENT_NETWORK_WIFI_NOTFOUND:
+  case PLUGIN_EVENT_NETWORK_WIFI_CONNECTING:
+  case PLUGIN_EVENT_NETWORK_WIFI_SOFTAP_CONNECTED:
+  case PLUGIN_EVENT_NETWORK_WIFI_FAIL:
+  case PLUGIN_EVENT_FAIL:
+  case PLUGIN_EVENT_OTA_START:
+  case PLUGIN_EVENT_OTA_END:
+  case PLUGIN_EVENT_OTA_ERROR:
+    break;
   }
 }
 
--- libraries/obniz/examples/online_led_on/online_led_on.ino
+++ libraries/obniz/examples/online_led_on/online_led_on.ino
@@ -13,6 +13,18 @@ void onEvent(os_event_t event, uint8_t* data, uint16_t length) {
     Serial.println("cloud Disconnected");
     onlineFlg = false;
     break;
+  case PLUGIN_EVENT_NETWORK_HARDWARE_CONNECTED:
+  case PLUGIN_EVENT_NETWORK_HARDWARE_DISCONNECTED:
+  case PLUGIN_EVENT_NETWORK_WIFI_SCANNING:
+  case PLUGIN_EVENT_NETWORK_WIFI_NOTFOUND:
+  case PLUGIN_EVENT_NETWORK_WIFI_CONNECTING:
+  case PLUGIN_EVENT_NETWORK_WIFI_SOFTAP_CONNECTED:
+  case PLUGIN_EVENT_NETWORK_WIFI_FAIL:
+  case PLUGIN_EVENT_FAIL:
+  case PLUGIN_EVENT_OTA_START:
+  case PLUGIN_EVENT_OTA_END:
+  case PLUGIN_EVENT_OTA_ERROR:
+    break;
   }
 }
 
